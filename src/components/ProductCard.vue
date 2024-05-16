9bcbd6b3a5608306da40c632dc22291629fa0477
Syntax0642 <calvinapriliohariyanto04@gmail.com>
2024-05-17 00:49:48 +0700

    feat [CA] main section

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="mt-6 product-card h-[900px] w-[500px]">
-    <img class="w-full h-[220px] rounded-lg" :src="data.productimg" alt="">
+  <div class="mt-10 product-card h-full w-[400px]">
+    <img class="w-full h-[220px] rounded-lg object-cover" :src="data.productimg" alt="">
     <div class="flex mt-4 gap-4 text-[20px] items-center">
       <font-awesome-icon class="text-black w-6 h-6 " :icon="faChild" />
-      <p class="font-normal text-2xl">Kost Pria</p>
+      <p class="font-normal text-[16px]">Kost Pria</p>
     </div>
     <div class="mt-3">
-      <h1 class="font-bold text-3xl">Berong-Kost 2 Kudus</h1>
+      <h1 class="font-bold text-[20px]">Berong-Kost 2 Kudus</h1>
     </div>
     <div class="mt-3">
-      <h1 class="font-normal text-2xl">Gebog,Kudus,Jawa Tengah</h1>
+      <h1 class="font-normal text-[15px]">Gebog,Kudus,Jawa Tengah</h1>
     </div>
     <div class="mt-3">
-      <h1 class="font-normal text-2xl">Mulai dari <span class="font-bold">Rp.1.300.000</span>/Bulan</h1>
+      <h1 class="font-normal text-[15px]">Mulai dari <span class="font-bold">Rp.1.300.000</span>/Bulan</h1>
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="bg-white p-0 m-0">
-<!--    <CarouselHome/>-->
-<!--    <div class="main ml-20 mt-10">-->
-<!--      <h1 class="font-extrabold text-3xl">Kos Terpopuler</h1>-->
-<!--      <ProductCard/>-->
-<!--    </div>-->
-    <ProfileCard/>
+    <Nav />
+    <CarouselHome />
+    <div class="main ml-20 mr-5 mt-20">
+      <h1 class="font-extrabold text-3xl">Kos Terpopuler</h1>
+      <div class="grid grid-cols-4 gap-20">
+        <ProductCard v-for="index in 4" :key="index" />
+      </div>
+      <div class="flex justify-center mt-20 mb-20">
+        <button class="bg-blue-primary flex items-center px-2 py-3 justify-center gap-5 w-[200px] rounded-lg text-white text-[22px] font-medium shadow-lg">
+          Lihat Semua
+        </button>
+      </div>
+      <h1 class="font-extrabold text-3xl">Cari Kos Sesuai Budgetmu</h1>
+      <PriceSortCard />
+      <div class="grid grid-cols-4 gap-4">
+        <ProductCard v-for="index in 8" :key="index" />
+      </div>
+      <div class="flex justify-center mt-20 mb-20">
+        <button class="bg-blue-primary flex items-center px-2 py-3 justify-center gap-5 w-[200px] rounded-lg text-white text-[22px] font-medium shadow-lg">
+          Lihat Semua
+        </button>
+      </div>>
+    </div>
   </div>
 </template>
 
@@ -13,5 +30,5 @@
 import Nav from "@/components/Nav.vue";
 import CarouselHome from "@/components/CarouselHome.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import ProfileCard from "@/components/ProfileCard.vue";
+import PriceSortCard from "@/components/PriceSortCard.vue";
 </script>
