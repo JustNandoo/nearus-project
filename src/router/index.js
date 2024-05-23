4ec1c8c980349d6c2fdd6ab735d1f6102f756ea6
JustNandoo <nandofajar1211@gmail.com>
2024-05-23 18:55:53 +0700

    feat [AF] RoutingDetailPage

--- src/router/index.js
+++ src/router/index.js
@@ -63,6 +63,11 @@ const router = createRouter({
       path: '/passworddata',
       name: 'passworddata',
       component: () => import ('../views/Profile/ProfilePassword.vue')
+    },
+    {
+      path: '/detail-kost',
+      name: 'detail-kost',
+      component: () => import ('../views/DetailPageView.vue')
     }
   ]
 })
