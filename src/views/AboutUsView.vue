d5f6cebcbe38baa7d910e429c612911755864176
JustNandoo <nandofajar1211@gmail.com>
2024-05-29 21:52:32 +0700

    feat [AF] AboutUsCard & OurTeam Card

---
+++ src/views/AboutUsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col items-center">
+    <AboutUsCard />
+    <CardProductAboutUs />
+    <TeamCard />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import AboutUsCard from "@/components/AboutUsCard.vue";
+import CardProductAboutUs from "@/components/CardProductAboutUs.vue";
+import Footer from "@/components/Footer.vue";
+import TeamCard from "@/components/TeamCard.vue";
+
+export default {
+  components: {
+    AboutUsCard,
+    CardProductAboutUs,
+    TeamCard,
+    Footer
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
