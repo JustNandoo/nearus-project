61132091b492f82ee4e0e56fa3e29f80755b8106
JustNandoo <nandofajar1211@gmail.com>
2024-05-29 23:47:21 +0700

    feat [AF] AboutUsText & Documentation Section

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[1561px] h-[554.19px] relative">
+  <div class="w-[1561px] h-[554.19px] top-40 relative">
     <div class="w-[330px] left-[617px] top-0 absolute text-sky-600 text-[40px] font-bold font-['Montserrat']">Meet The Team</div>
 
     <div class="w-[351px] h-[465.19px] absolute top-[86px] left-0 flex flex-col items-center">
@@ -26,7 +26,7 @@
 
     <div class="w-[351px] h-[465.19px] absolute top-[89px] left-[803px] flex flex-col items-center">
       <div class="w-[349.05px] h-[465.19px] bg-white rounded-[50px] shadow relative p-4 text-center">
-        <img class="w-[129.76px] h-[129.76px] rounded-full border-2 border-blue-95  0 mx-auto mt-4 mb-4" src="https://via.placeholder.com/130x130" />
+        <img class="w-[129.76px] h-[129.76px] rounded-full border-2 border-blue-950 mx-auto mt-4 mb-4" src="https://via.placeholder.com/130x130" />
         <div class="mb-4">
           <div class="text-neutral-800 text-xl font-bold font-['Montserrat']">Kenneth Pangestu</div>
           <div class="text-neutral-800 text-xl font-light font-['Montserrat'] mt-4">"Hidup ini terlalu singkat untuk membuang waktu pada orang yang tidak menghargai, memperhatikan, dan mencintaimu."</div>
--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -2,26 +2,33 @@
   <div class="flex flex-col items-center">
     <AboutUsCard />
     <CardProductAboutUs />
-    <TeamCard />
-    <Footer />
+    <AboutUsText />
+    <TeamCard class="mb-80"/>
+    <AboutUsDokumentasi /> 
+    <div class="w-[1777px] text-sky-600 text-4xl font-bold font-['Montserrat'] mt-40">"NeaRuS: Temukan Kost Terbaik, Nyaman, dan Terjangkau untuk Kehidupan yang Lebih Baik!"</div> <!-- Menambahkan margin-top -->
+    <Footer  /> 
   </div>
 </template>
 
 <script>
 import AboutUsCard from "@/components/AboutUsCard.vue";
 import CardProductAboutUs from "@/components/CardProductAboutUs.vue";
-import Footer from "@/components/Footer.vue";
+import AboutUsText from "@/components/AboutUsText.vue";
 import TeamCard from "@/components/TeamCard.vue";
+import AboutUsDokumentasi from "@/components/AboutUsDokumentasi.vue"; // Impor komponen AboutUsDokumentasi
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     AboutUsCard,
     CardProductAboutUs,
+    AboutUsText,
     TeamCard,
+    AboutUsDokumentasi, 
     Footer
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
