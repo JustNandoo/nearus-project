7037763b71391b93ba2afa7365432eebb14d0913
Syntax0642 <calvinapriliohariyanto04@gmail.com>
2024-08-29 07:56:54 +0700

    feat [CA] chat

--- src/views/Dashboard_pemilik/KoskuDashboard.vue
+++ src/views/Dashboard_pemilik/KoskuDashboard.vue
@@ -5,35 +5,35 @@
       <div class="max-w-4xl ">
         <div v-if="isLoading" class="text-gray-500">Loading...</div>
         <div v-else>
-          <div v-for="product in products" :key="product.id" class="border border-gray-300 rounded-lg p-4 mb-4 bg-white shadow-sm flex flex-col relative">
-            <div class="flex justify-between items-center">
-              <div class="flex items-center gap-4">
-                <img class="w-20 h-20 rounded-lg object-cover" :src="product.image[0]" alt="Product Image">
-                <div>
-                  <router-link :to="{ name: 'DetailProduct', params: { id: product.id } }" class="text-lg font-medium">
-                    {{ product.productname }}
-                  </router-link>
-                  <p class="text-gray-500">{{ product.location }}</p>
-                </div>
-              </div>
-              <font-awesome-icon
-                  :icon="faEllipsisV"
-                  class="text-gray-500 cursor-pointer"
-                  @click="toggleMenu(product.id)"
-              />
-              <div v-if="showMenu === product.id" class="absolute right-0 mt-8 bg-white shadow-md rounded-lg p-2">
-                <div class="cursor-pointer p-2 text-red-500" @click="deleteProduct(product.id)">Delete</div>
-              </div>
-            </div>
-            <hr class="my-2">
-            <div class="flex justify-between items-center text-gray-600">
-              <span>5 Penyewa</span>
-              <span>10 Kamar Tersedia</span>
-            </div>
-            <button class="mt-4 bg-blue-500 text-white rounded-full py-2 w-full hover:bg-blue-600 transition duration-300">
-              <a :href="`/dashboard-kosku-detail/${product.ownerId}`" class="block text-center">Manage</a>
-            </button>
-          </div>
+<!--          <div v-for="product in products" :key="product.id" class="border border-gray-300 rounded-lg p-4 mb-4 bg-white shadow-sm flex flex-col relative">-->
+<!--            <div class="flex justify-between items-center">-->
+<!--              <div class="flex items-center gap-4">-->
+<!--                <img class="w-20 h-20 rounded-lg object-cover" :src="product.image[0]" alt="Product Image">-->
+<!--                <div>-->
+<!--                  <router-link :to="{ name: 'DetailProduct', params: { id: product.id } }" class="text-lg font-medium">-->
+<!--                    {{ product.productname }}-->
+<!--                  </router-link>-->
+<!--                  <p class="text-gray-500">{{ product.location }}</p>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <font-awesome-icon-->
+<!--                  :icon="faEllipsisV"-->
+<!--                  class="text-gray-500 cursor-pointer"-->
+<!--                  @click="toggleMenu(product.id)"-->
+<!--              />-->
+<!--              <div v-if="showMenu === product.id" class="absolute right-0 mt-8 bg-white shadow-md rounded-lg p-2">-->
+<!--                <div class="cursor-pointer p-2 text-red-500" @click="deleteProduct(product.id)">Delete</div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <hr class="my-2">-->
+<!--            <div class="flex justify-between items-center text-gray-600">-->
+<!--              <span>5 Penyewa</span>-->
+<!--              <span>10 Kamar Tersedia</span>-->
+<!--            </div>-->
+<!--            <button class="mt-4 bg-blue-500 text-white rounded-full py-2 w-full hover:bg-blue-600 transition duration-300">-->
+<!--              <a :href="`/dashboard-kosku-detail/${product.ownerId}`" class="block text-center">Manage</a>-->
+<!--            </button>-->
+<!--          </div>-->
         </div>
       </div>
     </div>
